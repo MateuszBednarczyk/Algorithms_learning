2d5e9e46831ea44fe7320498b2c54a63828a1662
MateuszBednarczyk <m.bednarczyk@zsek.olsztyn.pl>
2023-09-11 16:34:35 +0200

    Hello message

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] data = {9,8,1,2,33,55,1};
-        bubbleSort(data);
-        for (int i : data) {
-            System.out.println(i);
-        }
-    }
-
-    public static void bubbleSort(int[] arr) {
-        for (int j = 0; j < arr.length; j++) {
-            for (int i = 0; i < arr.length - 1; i++) {
-                if (arr[i] > arr[i + 1]) {
-                    int lower = arr[i + 1];
-                    int higher = arr[i];
-
-                    arr[i] = lower;
-                    arr[i + 1] = higher;
-                }
-            }
-        }
+        System.out.println("Hello, that's repository where I'm going to put some algorithms implementations" +
+                "in couple of different languages");
     }
 }
