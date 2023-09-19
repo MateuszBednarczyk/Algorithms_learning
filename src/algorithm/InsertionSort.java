ff84527fdd58e03b2d2e2d153c560d899c12eb28
MateuszBednarczyk <m.bednarczyk@zsek.olsztyn.pl>
2023-09-19 21:23:15 +0200

    insertion sort

---
+++ src/algorithm/InsertionSort.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+public class InsertionSort {
+    public int[] Algorithm(int[] data) {
+        for (int i = 1; i < data.length; i++) {
+            int currentVal = data[i];
+
+            int j = i - 1;
+            while(j >= 0 && data[j] > currentVal) {
+                data[j + 1] = data[j];
+                j--;
+            }
+            data[j+1] = currentVal;
+        }
+        return data;
+    }
+}
---
+++ src/test/algorithm/InsertionSortTest.java
@@ -0,0 +1,30 @@
+package test.algorithm;
+
+import algorithm.InsertionSort;
+import algorithm.Type;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InsertionSortTest {
+        private final InsertionSort insertionSort = new InsertionSort();
+
+        public void InsertionSort() {
+            //given
+            List<int[][]> tests = new ArrayList<>();
+            tests.add(new int[][]{{100, 99, 1, 8},{1, 8, 99, 100}});
+            tests.add(new int[][]{{7, 10000, 2, 5, 91}, {2, 5, 7, 91, 10000}});
+
+            //when
+            //then
+            for (int[][] test : tests) {
+                int[] got = insertionSort.Algorithm(test[0]);
+                Test.checkTestStatus(test[1], got, new Object() {
+                }
+                        .getClass()
+                        .getEnclosingMethod()
+                        .getName(), Type.SORTING);
+            }
+        }
+
+}
