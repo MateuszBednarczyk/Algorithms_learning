4b13e23294d380e760b28763628d627b13b8d527
MateuszBednarczyk <m.bednarczyk@zsek.olsztyn.pl>
2023-09-22 19:41:25 +0200

    bubble sort fix

--- src/algorithm/BubbleSort.java
+++ src/algorithm/BubbleSort.java
@@ -3,6 +3,7 @@ package algorithm;
 public class BubbleSort {
     public int[] Algorithm(int[] data) {
         for (int j = 0; j < data.length; j++) {
+            boolean isElementSwapped = true;
             for (int i = 0; i < data.length - 1; i++) {
                 if (data[i] > data[i + 1]) {
                     int lower = data[i + 1];
@@ -10,7 +11,14 @@ public class BubbleSort {
 
                     data[i] = lower;
                     data[i + 1] = higher;
+                    isElementSwapped = true;
+                } else {
+                    isElementSwapped = false;
                 }
+            }
+
+            if (!isElementSwapped) {
+                break;
             }
         }
 
